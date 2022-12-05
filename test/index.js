c913f9c38f020e2b5a995a43973087d9c5605a10
anna-jonsson <anna.f.jonsson@gmail.com>
2022-12-05 15:19:23 +0100

    test folder and code
    some tests for the open api, test fetches etc.
    
    free to test!

---
+++ test/index.js
@@ -0,0 +1,46 @@
+/*
+
+READ: free to test!
+
+*/
+
+
+// Verified as working fetch call
+
+//EXAMPLE: Specific drink
+
+fetch(new Request("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"))
+    .then(r => r.json())
+    .then(rsc => console.log(rsc));
+
+
+//** EXAMPLE: Image for specific drink
+
+
+// fetch(new Request("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"))
+//     .then(r => r.json())
+//     .then(rsc => {
+//         let img = rsc.drinks[0].strDrinkThumb;
+//         document.querySelector('body').innerHTML = `<img src='${img}'>`;
+//     });
+
+
+// ** EXAMPLE: Random drink
+
+// fetch(new Request("https://www.thecocktaildb.com/api/json/v1/1/random.php"))
+//     .then(r => r.json())
+//     .then(rsc => {
+//         let img = rsc.drinks[0].strDrinkThumb;
+//         document.querySelector('body').innerHTML = `<img src='${img}'>`;
+//     });
+
+// ** EXAMPLE: FIlter drink by Specific ingredient
+
+fetch(new Request("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin"))
+    .then(r => r.json())
+    .then(rsc => console.log(rsc));
+
+let header = header();
+let footer = footer();
+
+let wrapper = "";
