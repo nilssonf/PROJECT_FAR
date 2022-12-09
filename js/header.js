417cfbc2d270cb16de2fb1ec98ef127bd678b5b5
anna-jonsson <anna.f.jonsson@gmail.com>
2022-12-09 14:15:52 +0100

    header working
    Co-Authored-By: rebeccamansson <97900349+rebeccamansson@users.noreply.github.com>

--- js/header.js
+++ js/header.js
@@ -4,34 +4,35 @@ function header() {
     let header = document.querySelector("#header");
     header.classList.add("header");
 
+    let icon;
+
     let logga = document.createElement("h2");
-    logga.innerText = "Bottled"
+    logga.innerText = "Bottled";
     logga.classList.add("logga");
 
     let scroll = document.createElement("p");
-    scroll.innerText = "Scroll Drinks"
+    scroll.innerText = "Scroll Drinks";
     scroll.classList.add("sroll");
 
     let about = document.createElement("p");
-    about.innerText = "About"
+    about.innerText = "About";
     about.classList.add("about");
 
     if (user == 0) {
-        let signIn = document.createElement("p");
-        signIn.innerText = "Sign in"
+        let signIn = document.createElement("div");
+        signIn.innerHTML = "<img src='../profiles/circle.png' class='circle'><p>Sign in</p>";
         signIn.classList.add("signIn");
-        header.append(logga, scroll, about, signIn)
+        header.append(logga, scroll, about, signIn);
     } else {
-
         let favourites = document.createElement("p");
-        favourites.innerText = "Favourite drinks"
-        favourites.classList.add("favourites")
+        favourites.innerText = "Favourite drinks";
+        favourites.classList.add("favourites");
 
-        let signOut = document.createElement("p");
-        signOut.innerText = "Sign out"
+        let signOut = document.createElement("div");
+        signOut.innerHTML = "<img src='../profiles/standard_picture.png' class='standard'><p>Sign out</p>";
         signOut.classList.add("signOut");
 
-        header.append(logga, scroll, about, favourites, signOut)
+        header.append(logga, scroll, about, favourites, signOut);
     }
 
 
@@ -39,21 +40,21 @@ function header() {
 
 
 
-    logga.onclick = function() {
-        location.href = 'index.html'
-    }
+    logga.onclick = function () {
+        location.href = 'index.html';
+    };
 
-    scroll.onclick = function() {
-        location.href = '../html/search.html'
-    }
+    scroll.onclick = function () {
+        location.href = '../html/search.html';
+    };
 
-    about.onclick = function() {
-        location.href = '../html/about.html'
-    }
+    about.onclick = function () {
+        location.href = '../html/about.html';
+    };
 
 
 }
 
 
 
-header()
+header();
