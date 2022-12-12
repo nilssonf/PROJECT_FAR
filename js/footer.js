12310af55c2375a8fed1a8cde910762b3f52ec51
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-12-12 10:50:33 +0100

    Updated fonts in header + footer
    Correct fonts for title and menu

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+<<<<<<< Updated upstream
+=======
+"use strict";
+
+function buildFooter() {
+    let footer = document.querySelector("#footer");
+    footer.classList.add("footer");
+    footer.innerText = "Bottled";
+}
+
+buildFooter()
+>>>>>>> Stashed changes
