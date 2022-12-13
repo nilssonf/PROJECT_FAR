e926f78614dfad5f387113c82b3b282c3e427e79
anna-jonsson <anna.f.jonsson@gmail.com>
2022-12-13 15:47:39 +0100

    fixes

--- js/header.js
+++ js/header.js
@@ -8,38 +8,42 @@ function header() {
 
     let logga = document.createElement("h2");
     logga.innerText = "Bottled";
-    logga.classList.add("logga");
+    logga.classList.add("logga", "menuElm");
 
     let scroll = document.createElement("p");
     scroll.innerText = "Scroll Drinks";
-    scroll.classList.add("scroll");
+    scroll.classList.add("scroll", "menuElm");
 
     let about = document.createElement("p");
     about.innerText = "About";
-    about.classList.add("about");
+    about.classList.add("about", "menuElm");
 
     if (user == 0) {
         let signIn = document.createElement("div");
-        signIn.innerHTML = "<img src='../profiles/circle.png' class='circle'><p>Sign in</p>";
+        signIn.innerHTML = "<img src='../profiles/circle.png' class='circle'><p class='menuElm'>Sign in</p>";
         signIn.classList.add("signIn");
         header.append(logga, scroll, about, signIn);
     } else {
         let favourites = document.createElement("p");
         favourites.innerText = "Favourite drinks";
-        favourites.classList.add("favourites");
+        favourites.classList.add("favourites", "menuElm");
 
         let signOut = document.createElement("div");
-        signOut.innerHTML = "<img src='../profiles/standard_picture.png' class='standard'><p>Sign out</p>";
-        signOut.classList.add("signOut");
+        signOut.innerHTML = "<img src='../profiles/standard_picture.png' class='standard profile'><p>Sign out</p>";
+        signOut.classList.add("signOut", "menuElm");
 
         header.append(logga, scroll, about, favourites, signOut);
 
-
-        favourites.onclick = function() {
+        favourites.onclick = function () {
             location.href = '../html/favorites.html';
         };
-    }
 
+        let profilePic = document.querySelector('.profile');
+
+        profilePic.onclick = function () {
+            location.href = '../html/profile.html';
+        };
+    }
 
     logga.onclick = function () {
         location.href = 'index.html';
@@ -54,7 +58,5 @@ function header() {
     };
 
 }
-
-
 
 header();
