e1e5ed61e7cc621086cdd47c172de1fdac82f317
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-12-14 11:21:15 +0100

    Fixed random pictures on index-page
    Random drinks and ingredients

--- js/index.js
+++ js/index.js
@@ -1,106 +1,73 @@
-"use strict";
+'use strict'
 
-let user = 1;
+let user = 1
 
-window.onload = getRandomIngredients();
+window.onload = getRandomIngredients()
 
-window.onload = getRandomDrinks();
+window.onload = getRandomDrinks()
 
+function getRandomIngredients () {
+  const rqstIngredients = new Request('../php/topIngredients.json')
 
-function getRandomIngredients() {
+  fetch(rqstIngredients)
+    .then(r => r.json())
+    .then(ingredients => {
+      const n = 3
+      let ingredient3 = ingredients
+        .map(x => ({ x, r: Math.random() }))
+        .sort((a, b) => a.r - b.r)
+        .map(a => a.x)
+        .slice(0, n)
 
-    const rqstIngredients = new Request("../php/topIngredients.json");
-
-    fetch(rqstIngredients)
-        .then(r => r.json())
-        .then(ingredients => {
-            const n = 3;
-            let ingredient3 = ingredients
-            .map(x => ({ x, r: Math.random() }))
-            .sort((a, b) => a.r - b.r)
-            .map(a => a.x)
-            .slice(0, n);
-            console.log(ingredient3)
-
-            showTopIngredients(ingredient3);
-
-        });
-
+      showTopIngredients(ingredient3)
+    })
 }
 
-function getRandomDrinks() {
+function getRandomDrinks () {
+  const rqstDrinks = new Request('../php/topDrinks.json')
 
-    const rqstDrinks = new Request("../php/topDrinks.json");
+  fetch(rqstDrinks)
+    .then(r => r.json())
+    .then(drinks => {
+      const n = 3
+      let drink3 = drinks
+        .map(x => ({ x, r: Math.random() }))
+        .sort((a, b) => a.r - b.r)
+        .map(a => a.x)
+        .slice(0, n)
 
-    fetch(rqstDrinks)
-        .then(r => r.json())
-        .then(drinks => {
-            const n = 3;
-            let drink3 = drinks
-            .map(x => ({ x, r: Math.random() }))
-            .sort((a, b) => a.r - b.r)
-            .map(a => a.x)
-            .slice(0, n);
-            console.log(drink3)
-
-            showTopDrinks(drink3)
-        });
+      showTopDrinks(drink3)
+    })
 }
 
-function showTopDrinks (drink3){
+function showTopDrinks (drink3) {
+  const numbers = ['One', 'Two', 'Three']
 
-
-    let drinks = [];
-
-    drink3.forEach(drink => {
-       
-    
-        fetch(new Request(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.drinkId}`))
-        .then(r => r.json())
-        .then(rsc => {
-            
-            drinks.push(`${rsc.drinks[0].strDrinkThumb}`); 
-            
-            
-            
-        });
-
-
-
-    });
-
-    console.log(drinks[0]);
-   let topDrinkOne = document.createElement("img")
-   
-   topDrinkOne.src = drinks[0];
-
-   document.querySelector(".drinkOne").append(topDrinkOne);
-
-   console.log(topDrinkOne);
-
-  
-
-   
+  drink3.forEach((drink, index) => {
+    fetch(
+      new Request(
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.drinkId}`
+      )
+    )
+      .then(r => r.json())
+      .then(rsc => {
+        let class_name = '.drink' + numbers[index]
+        let topDrinkOne = document.createElement('img')
+        topDrinkOne.classList.add('round', 'top')
+        topDrinkOne.src = rsc.drinks[0].strDrinkThumb
+        document.querySelector(class_name).append(topDrinkOne)
+      })
+  })
 }
 
-// function showTopIngredients (ingredient3){
-//     let ingredients = [];
+function showTopIngredients (ingredient3) {
+  const numbers = ['One', 'Two', 'Three']
 
-//    ingredient3.forEach(ingredient => {
-       
-    
-//         fetch(new Request(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${ingredient.ingId}`))
-//         .then(r => r.json())
-//         .then(rsc => {
-            
-//             document.createElement("img").src = `${rsc.drinks[0].strDrinkThumb}`; 
-            
-            
-            
-//         });
-
-
-
-//     });
-    
-// }
+  ingredient3.forEach((ingredient, index) => {
+    let class_name = '.ingredient' + numbers[index]
+    let topIngredientOne = document.createElement('img')
+    topIngredientOne.classList.add('top')
+    topIngredientOne.src = ingredient.ingImg
+    document.querySelector(class_name).append(topIngredientOne)
+  })
+}
