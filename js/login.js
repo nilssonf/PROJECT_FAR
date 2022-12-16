2da992c44a997c36ef484bba0ee679ae158ed985
anna-jonsson <anna.f.jonsson@gmail.com>
2022-12-16 12:43:43 +0100

    checks for which user is logged in
    changes user variable to the user's (in users.json) ID.

--- js/login.js
+++ js/login.js
@@ -1,5 +1,23 @@
 "use strict";
 
-function createLogin() {
-    console.log("hej")
+let user = 1;
+
+function logIn(username, pw) {
+
+    let userObj = {
+        email: username,
+        password: pw
+    };
+
+    const logInRqst = new Request("../php/logIn.php", {
+        method: 'POST',
+        body: JSON.stringify(userObj),
+        headers: { "Content-type": "application/json" }
+    });
+
+    fetch(logInRqst)
+        .then(resp => resp.json())
+        .then(rsc => {
+            user = rsc.id;
+        });
 }
